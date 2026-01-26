58562f5547dd67db9fd8582c7513fb0465910c8c
sunche51 <chengyi.sun@mail.utoronto.ca>
2026-01-26 16:36:24 -0500

    preparing for containerization

---
+++ db/init/001_bootstrap.sql
@@ -0,0 +1,11 @@
+\i /sql/schema/001_init.sql
+
+\i /sql/migrations/001_create_customers.sql
+\i /sql/migrations/002_create_suppliers.sql
+\i /sql/migrations/003_create_items.sql
+\i /sql/migrations/004_create_warehouses.sql
+\i /sql/migrations/005_create_inventory_transactions.sql
+\i /sql/migrations/007_create_purchase_orders.sql
+\i /sql/migrations/008_create_purchase_order_lines.sql
+\i /sql/migrations/011_create_sales_orders.sql
+\i /sql/migrations/012_create_sales_order_lines.sql
